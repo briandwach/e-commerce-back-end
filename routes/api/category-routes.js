f00cece0a12f2501549d9c67b739c2aff75577a6
livefromthelevel <brian.d.wach@gmail.com>
2024-03-19 20:37:58 -0500

    <Completed route logic for requests to the category API>

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -32,16 +32,42 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const addCategory = await Category.create(req.body);
+    res.status(200).json(addCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updateCategory = await Category.update(
+      { category_name: req.body.category_name },
+      { where: { id: req.params.id } });
+
+    res.status(200).json(updateCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deleteCategory = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    res.status(200).json(deleteCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
